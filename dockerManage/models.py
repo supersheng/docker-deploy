d1f763b93289e39d7c6a6ae192a02bbc8e28b905
pc <pc@51reboot.com>
2016-10-30 15:57:17 +0800

    first commit

---
+++ dockerManage/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class Instance(models.Model):
+    domain = models.CharField(max_length=256)
+    docker_id = models.CharField(max_length=128)
+    ip = models.GenericIPAddressField(protocol='IPv4')
+    port = models.IntegerField()
