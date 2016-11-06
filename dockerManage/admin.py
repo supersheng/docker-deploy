76d50fcfc17fb9021a75c8b4f22c79673ce9ca83
pc <pc@51reboot.com>
2016-11-06 10:06:24 +0800

    reg InstanceAdmin

--- dockerManage/admin.py
+++ dockerManage/admin.py
@@ -4,6 +4,11 @@ from dockerManage.models import Instance
 class InstanceAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         print obj, form
+        obj.port = 1234
+        obj.save()
 
-admin.site.register(Instance)
+    def delete_model(self, request, obj):
+        print obj.port
+        obj.delete()
+admin.site.register(Instance, InstanceAdmin)
 
